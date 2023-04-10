1c44eee17b51bbe7dac653d98e1f8d8913a5cdf0
xx-Flex-xx <endlessxxxpain@gmail.com>
2023-04-11 01:29:29 +0300

    add comments full

--- src/middlewares/auth/auth.middleware.ts
+++ src/middlewares/auth/auth.middleware.ts
@@ -3,6 +3,8 @@ import {jwtService} from "../../application/jwtService";
 import {usersService} from "../../domain/users.service";
 import jwt from "jsonwebtoken";
 import {usersCollection} from "../../dataBase/db.posts.and.blogs";
+import {usersRepositories} from "../../repositories/users.repositories";
+import {authUsersRepositories} from "../../repositories/auth.users.repositories";
 
 //super admin check
 const expressBasicAuth = require('express-basic-auth');
@@ -13,23 +15,33 @@ export const adminStatusAuth = expressBasicAuth({users: {'admin': 'qwerty'}});
 //check token
 export const authMiddleware = async (req: Request, res: Response, next: NextFunction) =>
 {
-   if(!req.headers.authorization) {
-       console.log("1")
-       res.sendStatus(401)
-       return
-   }
-   const token = req.headers.authorization.split(' ')[1]
+    if(!req.headers.authorization)
+    {
+        console.log("1")
+        res.sendStatus(401)
+        return
+    }
+    const token = req.headers.authorization.split(' ')[1]
 
-    try{
-        const decode: any = jwt.verify(token, '34343434')
-        const user = await usersService.findUserById(decode.userId)
-        if(!user) {
-            console.log("2")
-            res.sendStatus(404)
-            return
+    try
+    {
+        const IsDecode: any = jwt.verify(token, '34343434')
+
+        if(IsDecode)
+        {
+            const user = await authUsersRepositories.getUser(token)
+
+            if(user === null)
+            {
+                console.log("2")
+                res.sendStatus(404)
+                return
+            }
+            req.user = user
         }
-        req.user = user
-    } catch (e) {
+    }
+   catch (e)
+    {
         console.log(e)
         console.log("3")
         res.sendStatus(401)
