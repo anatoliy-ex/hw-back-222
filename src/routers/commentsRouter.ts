6c726ba23f010fcb7a4ab7c5955ad59051fb26e4
xx-Flex-xx <endlessxxxpain@gmail.com>
2023-06-13 19:28:14 +0300

    hw 11 - add new db

--- src/routers/commentsRouter.ts
+++ src/routers/commentsRouter.ts
@@ -9,7 +9,7 @@ import {commentController} from "../roots/composition.root";
 export const commentsRouter = Router({});
 
 //like and dislike status
-commentsRouter.put('/:commentId/like-status', authMiddleware, inputValidationMiddleware , commentController.LikeAndDislikeStatus.bind(commentController));
+commentsRouter.put('/:commentId/like-status', authMiddleware, likeStatusValidator, inputValidationMiddleware , commentController.LikeAndDislikeStatus.bind(commentController));
 
 //update comment by ID
 commentsRouter.put('/:commentId', authMiddleware, checkForUser, contentCommentValidator, inputValidationMiddleware , commentController.UpdateCommentById.bind(commentController));
